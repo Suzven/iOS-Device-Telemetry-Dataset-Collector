6ced0a973a9cbead405ce4b86f042ee4d781570c
Suzven <nevzus@gmail.com>
2026-09-10 16:10:04 +0300

    added base proj

---
+++ iOS-Device-Telemetry-Dataset-Collector/iOS_Device_Telemetry_Dataset_CollectorApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_Device_Telemetry_Dataset_CollectorApp.swift
+//  iOS-Device-Telemetry-Dataset-Collector
+//
+//  Created by Алексей Коваленко on 10.09.2026.
+//
+
+import SwiftUI
+
+@main
+struct iOS_Device_Telemetry_Dataset_CollectorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
